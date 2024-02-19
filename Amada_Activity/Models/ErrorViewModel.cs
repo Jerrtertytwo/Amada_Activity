41a814e7f67f7e5a929ab59d7a50982969848b21
amada <amada@JERRYAMADA>
2024-02-19 09:11:09 +0800

    Add project files.

---
+++ Amada_Activity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Amada_Activity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
